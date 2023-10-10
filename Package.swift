30089db7ee0aa53e4990473c66c48901ec891085
Hideaki Tanabe <tanablog@gmail.com>
2023-10-10 14:50:36 +0900

    Update dependent libraries

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/apple/swift-argument-parser",
-            from: "1.1.0"),
+            from: "1.2.3"),
     ],
     targets: [
         .target(
